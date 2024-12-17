8c9e28dc8ad74760d56feb8d124f6680d045c68a
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2024-12-17 19:18:57 +0900

    Title: 별 그리기, Time: 72ms, Memory: 18MB, Status: Passed - CodeTree

--- 241217/별 그리기/star-drawing.py
+++ 241217/별 그리기/star-drawing.py
@@ -18,7 +18,7 @@ for i in range(n): # 0  1 2
 for i in range(n):
     for j in range(i+1):
         print(" ",end="")
-    for j in range(n-i*2):
+    for j in range(2*n-i*2-3):
         print("*",end="")
     
     print()
