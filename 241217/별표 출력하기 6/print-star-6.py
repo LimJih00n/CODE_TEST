0cb944c1b3bc42574bc78909f2d25ba38870d4cf
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2024-12-17 19:05:57 +0900

    Title: 별표 출력하기 6, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241217/별표 출력하기 6/print-star-6.py
+++ 241217/별표 출력하기 6/print-star-6.py
@@ -19,9 +19,18 @@ for i in range(n):
     for j in range((n*2-1)-2*i,0,-1):
         print("* ",end="")
     print("")
-for i in range(n-1): 
-    for j in range(n-(i*2)): #4 2 0 0 2 4
+# 6 => 4 3 2 1 0
+# 8이여야함 
+#  2 1 0
+#4 4 2 0
+#5 6 4 2 0
+#  0 1 2 3 4
+#6 8 6 4 2 0
+#     5 4 3 2 1 0
+#7 10 8 6 4 2 0
+for i in range(n-2,-1,-1): # 2 1 0 n = 4
+    for j in range(i*2): #4 2 0
         print(" ",end="")
-    for j in range(i*2+3):
+    for j in range((n-i-1)*2+1): # 3 5 7 => 2*i+1 (n-i)*2+1
         print("* ",end="")
     print("")
