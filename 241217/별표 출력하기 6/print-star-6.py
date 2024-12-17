eb91be993fa162bc9671150edefd7bc04cbf0283
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2024-12-17 18:36:55 +0900

    Title: 별표 출력하기 6, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241217/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,27 @@
+'''
+
+3 0 7 
+2 2 5
+1 4 3
+
+3 6 1
+
+0 4 3 
+1 2 5
+2 0 7
+
+n = 4
+'''
+n = int(input())
+for i in range(n):
+    for j in range(i*2):
+        print(" ",end="")
+    for j in range((n*2-1)-2*i,0,-1):
+        print("* ",end="")
+    print("")
+for i in range(n,-1,-2): 
+    for j in range(i):
+        print(" ",end="")
+    for j in range(i,n*2-1):
+        print("* ",end="")
+    print("")
