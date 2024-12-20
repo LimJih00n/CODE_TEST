9af1099fb1e655e0cbef51faaa2c2f2d1f26f610
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2024-12-20 19:03:26 +0900

    Title: 알파벳 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241220/알파벳 출력하기/print-alphabet.py
+++ 241220/알파벳 출력하기/print-alphabet.py
@@ -4,5 +4,5 @@ n = int(input())
 for i in range(n):
     for j in range(i+1):
         print(chr(65+cnt),end="")
-        cnt = cnt +1 if chr(65+cnt) != "Z" else 1
+        cnt = cnt +1 if chr(65+cnt) != "Z" else 0
     print()
