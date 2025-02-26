6a6948218926b4c586135f85904af3fb8913fad7
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-02-26 19:14:22 +0900

    Title: 코로나 메뉴얼2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250226/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,18 @@
+per = []
+hes = [0]*4
+for i in range(3):
+    per.append(list(input().split()))
+for v in per:
+    if int(v[1])>=37 and v[0]=='Y':
+        hes[0]+=1
+    elif int(v[1])>=37 and v[0]=='N':
+        hes[1]+=1
+    elif int(v[1])<37 and v[0]=='Y':
+        hes[2]+=1
+    else:
+        hes[3]+=1
+if sum(hes)>2:
+    hes.append("E")
+for n in hes:
+    print(n,end=" ")
+
