65e0285a8dae7122d49ff50153467bf7aff680b8
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-02-27 17:44:27 +0900

    Title: 왔다 갔던 구역 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250227/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,25 @@
+n = int(input())
+x = []
+dir = []
+commands = []
+for _ in range(n):
+    xi, di = input().split()
+    commands.append((xi,di))
+    
+
+# Please write your code here.
+arr = [0] * 2001
+cur = 0
+for command in commands:
+    x = int(command[0])
+    if command[1] == "R":
+        for idx in range(cur,cur+x):
+            arr[idx+1000]+=1
+        cur = cur+x
+    elif command[1] == "L":
+        for idx in range(cur,cur-x,-1):
+            arr[idx+1000]+=1
+        cur = cur-x
+
+ans = [1 for n in arr if n>=2]
+print(sum(ans))
