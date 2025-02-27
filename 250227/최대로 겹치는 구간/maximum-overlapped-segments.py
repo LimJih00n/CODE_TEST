dfb0ee082481105255018818db52828a00eb0a46
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-02-27 16:48:24 +0900

    Title: 최대로 겹치는 구간, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250227/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -2,9 +2,10 @@ n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-arr = [0]*202
+arr = [0]*201
 for x1,x2 in segments:
-    for idx in range(x1+100,x2+101):
-        if not (idx==x1 or idx== x2):
-            arr[idx] +=1
+    for idx in range(x1+100,x2+100):
+        
+        arr[idx] +=1
+        
 print(max(arr))
