c333fd637f1cde0a9ff281e39a7330cc7bdd1edf
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-02-27 17:30:36 +0900

    Title: DateTime to DateTime, Time: 39ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250227/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,11 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+day = 11 
+hour = 11
+mint = 11
+
+cur_state =  11*24*60+11*60+11 
+goal_state = a*24*60+b*60+c 
+ans = goal_state-cur_state if goal_state-cur_state >0 else -1
+print(goal_state-cur_state)
