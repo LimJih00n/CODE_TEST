e042a483aa3e98c48c94e797319ea9f79e008623
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-02-27 18:06:15 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250227/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -8,6 +8,8 @@ for _ in range(n):
     
 
 # Please write your code here.
+#  0 1 2 3
+# 0~1~2~3~4
 arr = [0] * 2001
 cur = 0
 for command in commands:
@@ -15,11 +17,11 @@ for command in commands:
     if command[1] == "R":
         for idx in range(cur,cur+x):
             arr[idx+1000]+=1
-        cur = cur+x+1
+        cur = cur+x
     elif command[1] == "L":
-        for idx in range(cur,cur-x,-1):
+        for idx in range(cur-1,cur-x-1,-1):
             arr[idx+1000]+=1
-        cur = cur-x-1
+        cur = cur-x
 
 ans = [1 for n in arr if n>=2]
 print(sum(ans))
