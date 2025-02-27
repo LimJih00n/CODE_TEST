3974e6ffc64f16cf25d2d047bd20c82641b8ca69
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-02-27 17:31:08 +0900

    Title: DateTime to DateTime, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250227/DateTime to DateTime/datetime-to-datetime.py
+++ 250227/DateTime to DateTime/datetime-to-datetime.py
@@ -7,5 +7,5 @@ mint = 11
 
 cur_state =  11*24*60+11*60+11 
 goal_state = a*24*60+b*60+c 
-ans = goal_state-cur_state if goal_state-cur_state >0 else -1
+ans = goal_state-cur_state if goal_state-cur_state >=0 else -1
 print(goal_state-cur_state)
