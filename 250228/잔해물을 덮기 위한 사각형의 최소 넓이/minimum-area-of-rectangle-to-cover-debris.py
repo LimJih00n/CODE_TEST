994b6e16e310e2d1325eda5ff96cf5acd9528c17
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-02-28 20:34:19 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 128ms, Memory: 49MB, Status: Passed - CodeTree

--- 250228/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250228/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -18,17 +18,19 @@ p1x = float('inf')
 p1y = float('inf')
 p2x = float('-inf')
 p2y = float('-inf')
-
+area = 0
 for r in range(len(arr)):
     for c in range(len(arr[r])):
         if arr[r][c] ==2:
+            area =1
             p1x = min(p1x,r)
             p2x = max(p2x,r)
             p1y = min(p1y,c)
             p2y = max(p2y,c)
 
-
-if x1[0] == x1[1] and x2[0] == x2[1] and y1[0] == y1[1] and  y2[0] == y2[1]:
+if area == 0:
+    print(0)
+elif x1[0] == x1[1] and x2[0] == x2[1] and y1[0] == y1[1] and  y2[0] == y2[1]:
     print(0)
 elif p1x==p2x and p1y==p2y:
     print(0)
