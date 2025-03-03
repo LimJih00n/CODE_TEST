ee8fecb78033eb2bafb83ed91d900dff8f981580
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-03-03 15:35:36 +0900

    Title: 문자에 따른 명령 2, Time: 61ms, Memory: 19MB, Status: Passed - CodeTree

--- 250303/문자에 따른 명령 2/text-based-commands2.py
+++ 250303/문자에 따른 명령 2/text-based-commands2.py
@@ -10,7 +10,7 @@ for c in command:
     if c == "L":
         head_dir = head_dir-1 if head_dir>0 else 3
     elif c=="R":
-        head_dir = head_dir+1 if head_dir>2 else 0
+        head_dir = head_dir+1 if head_dir<3 else 0
     else:
         start[0] += move_dir[head_dir][0]
         start[1] += move_dir[head_dir][1]
