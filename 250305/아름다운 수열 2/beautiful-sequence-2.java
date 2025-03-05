9e2e045800c87fb1d8354365d76100fcea2d48f0
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-03-05 18:22:12 +0900

    Title: 아름다운 수열 2, Time: 130ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 250305/아름다운 수열 2/beautiful-sequence-2.java
+++ 250305/아름다운 수열 2/beautiful-sequence-2.java
@@ -1,44 +1,49 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
         int[] A = new int[N];
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
+        }
         int[] B = new int[M];
-        for (int i = 0; i < M; i++)
+        for (int i = 0; i < M; i++) {
             B[i] = sc.nextInt();
-        // Please write your code here.
+        }
+
+        Map<Integer, Integer> bFreq = new HashMap<>();
+        for (int num : B) {
+            bFreq.put(num, bFreq.getOrDefault(num, 0) + 1);
+        }
+
         int ans = 0;
-        for (int i =0;i<N;i++){
-            int[] arr = new int[102];
-        
-            int c = 0;
-            for(int j=i;j<i+M;j++){
-                if (j>=N){
-                    break;
-                }
-                c++;
-                arr[A[j]] = 1;        
+        Map<Integer, Integer> windowFreq = new HashMap<>();
+        for (int i = 0; i < M; i++) {
+            windowFreq.put(A[i], windowFreq.getOrDefault(A[i], 0) + 1);
+        }
+
+        if (windowFreq.equals(bFreq)) {
+            ans++;
+        }
+
+        for (int i = M; i < N; i++) {
+            int oldVal = A[i - M];
+            windowFreq.put(oldVal, windowFreq.get(oldVal) - 1);
+            if (windowFreq.get(oldVal) == 0) {
+                windowFreq.remove(oldVal);
             }
-            if (c != M){
-                continue;
-            }
-            int count= 0;
-            for(int j=0;j<M;j++){
-                    if(arr[B[j]]==1){
-                        count++;
-                    }
-            }
-            //System.out.println(count);
-            if(count == M){
-                ans ++;
+
+            int newVal = A[i];
+            windowFreq.put(newVal, windowFreq.getOrDefault(newVal, 0) + 1);
+
+            if (windowFreq.equals(bFreq)) {
+                ans++;
             }
         }
+
         System.out.println(ans);
-
-
     }
 }
