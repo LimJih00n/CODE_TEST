780c46432f9beaadc819cd058f54417bcadf4a36
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-03-06 15:58:26 +0900

    Title: 바구니 안의 사탕 2, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250306/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250306/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -13,11 +13,12 @@ what? 특정 범위 이내의 사탕 개수를 합의 최대를 구하는 것.
 how? 1. 사탕 배열 만들기 => 0은 없음 n은 있음 idx가 좌표
      2. 처음 부터 끝까지 돌면서 +- k 의 case를 합하기
      3. max 찾기
+    cf: 같은 위치에 여러개가 놓여있을 수도 있음
 
 '''
 candys = [0]*101
 for c,p in zip(candy,pos):
-    candys[p] = c
+    candys[p] += c
 ans = 0
 for i in range(101):
     k_ = k if i- k >=0 else i 
