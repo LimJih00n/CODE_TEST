4b6f4c7cf0c7773f764f246ec709ff60a92b8916
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-03-06 15:51:35 +0900

    Title: 씨 오 더블유 2, Time: 70ms, Memory: 19MB, Status: Passed - CodeTree

--- 250306/씨 오 더블유 2/c-o-w-2.py
+++ 250306/씨 오 더블유 2/c-o-w-2.py
@@ -9,23 +9,22 @@ how: 가능한 모든 조합 만들고 COW인지 확인하기.
 '''
 def gen_p(elements,r):
     re = []
-    visits = [0] * len(elements)
-    def dfs(path):
+    
+    def dfs(path,s):
         if len(path) == r:
             re.append(path[:])
             return
-        for i in range(len(elements)):
-            if not visits[i]:
-                visits[i] = 1
-                path.append(elements[i])
-                dfs(path)
-                path.pop()
-                visits[i] = 0
-    dfs([])
+        for i in range(s,len(elements)):
+            path.append(elements[i])
+            dfs(path,i+1)
+            path.pop()
+            
+    dfs([],0)
     return re
 elements = [i for i in range(n)]
 ans = 0
 cases = gen_p(elements,3)
+#print(cases)
 for c in cases:
     if (arr[c[0]]+arr[c[1]]+arr[c[2]]) == "COW":
         ans +=1
