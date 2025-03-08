5b8b9ba3819e92eb8ebe364e64527a9e2e548aae
LimJih00n <142905223+LimJih00n@users.noreply.github.com>
2025-03-08 19:46:18 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 52ms, Memory: 17MB, Status: Passed - CodeTree

--- 250308/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 250308/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -7,44 +7,68 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 marbles = [tuple(map(int, input().split())) for _ in range(m)]
 r = [pos[0]-1 for pos in marbles]
 c = [pos[1]-1 for pos in marbles]
-marbles = [(r1, c1) for r1, c1 in zip(r, c)]
+marbles = [(r1,c1) for r1,c1 in zip(r,c) ]
 
-# 이동 방향 (상, 하, 좌, 우 순서)
+# Please write your code here.
+'''
+what? 구슬 움직임 => 상하좌우를 기준으로 높은 칸으로 이동. 이동 우선순위 따름
+"그냥 큰게 아니라 가장 큰 값임"
+이동 후 충돌 시 구슬은 사라짐 T초 후 남아있는 구슬의 수를 구하기. 
+문제 잘보기 "가장큰"
+how?
+1.구슬일 경우 이동시키기.
+2.이동 나타내는 배열 만들고. 겹치면 초기화
+1,2반복
+'''
+
+def check_b(r,c):
+    if r>=0 and c>=0 and r<n and c<n:
+        return True
+    return False
 move_dir = [
-    (-1, 0),  # 상
-    (1, 0),   # 하
-    (0, -1),  # 좌
-    (0, 1)    # 우
+    (-1,0),
+    (1,0),
+    (0,-1),
+    (0,1)
 ]
+m_map = [[0] * n for i in range(n)]
+for r,c in zip(r,c):
+    m_map[r][c] = 1
+# m_map 초기화
+ans = 0
 
-# 경계 체크 함수
-def check_b(r, c):
-    return 0 <= r < n and 0 <= c < n
+while True:
+    can_move=[]
+    remove_mable = []
+    for marble in marbles:
+        mr,mc = marble
+        max_v,move_p = 0,(0,0)
+        for i in range(4):
+        
+        
+            n_mr = mr+move_dir[i][0]
+            n_mc = mc+move_dir[i][1]
+            
+            # 8 8 5 8 8 
+            if check_b(n_mr,n_mc) and arr[n_mr][n_mc] > max_v:
+                
+                max_v = arr[n_mr][n_mc]
+                move_p = (n_mr,n_mc)
 
-# t초 동안 이동 실행
-for _ in range(t):
-    new_positions = {}  # 이동 후 위치 저장
-    move_count = {}  # 위치별 구슬 개수 체크
+        if move_p in can_move:#중복되는 경우 
+            can_move.remove(move_p) #제거 
+            remove_mable.append(move_p) # 그 좌표는 다 충돌임
+        elif move_p not in remove_mable: #문제 같은 게 또 들어오면 오류 나옴 - 3개가 겹칠 수 있음
+            can_move.append(move_p)
+        
+            
+                
+    marbles = can_move[:]
+    t -= 1
+    
+    if t==0:
+        ans = len(can_move)
+        break
 
-    for mr, mc in marbles:
-        max_val = arr[mr][mc]
-        move_p = (mr, mc)
+print(ans)
 
-        for dr, dc in move_dir:
-            nr, nc = mr + dr, mc + dc
-            if check_b(nr, nc) and arr[nr][nc] > max_val:
-                max_val = arr[nr][nc]
-                move_p = (nr, nc)
-
-        # 이동 위치 저장 (딕셔너리 사용)
-        if move_p not in new_positions:
-            new_positions[move_p] = (mr, mc)
-            move_count[move_p] = 1
-        else:
-            move_count[move_p] += 1  # 충돌 발생
-
-    # 충돌하지 않은 구슬만 남기기
-    marbles = [new_positions[pos] for pos in new_positions if move_count[pos] == 1]
-
-# 남아있는 구슬 개수 출력
-print(len(marbles))
